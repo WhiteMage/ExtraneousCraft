94ef2b688d5406761c77357edc17da6bc51ea37a
Astorian <royalgamingarmy@gmail.com>
2013-03-09 12:38:49 +0200

    Initial Commit on Addons and Startup

---
+++ ExtraneousCraft/common/exc/addon/AddonIC2.java
@@ -0,0 +1,5 @@
+package exc.addon;
+
+public class AddonIC2 {
+
+}
---
+++ ExtraneousCraft/common/exc/core/helper/StartupHelper.java
@@ -0,0 +1,12 @@
+package exc.core.helper;
+
+public class StartupHelper { 
+	
+	public static void initAddonCheck() {
+		CheckHelper.checkIC2();
+		CheckHelper.checkBC();
+	}
+	
+	
+
+}
