955ccd8533defd1e1b12922cfdbfa00e27b66cc5
Astorian <royalgamingarmy@gmail.com>
2013-03-15 15:02:37 +0200

    Ended up rounding back the placeholders it just bugged me so much

--- common/astorian/exc/core/helper/RecipeHelper.java
+++ common/astorian/exc/core/helper/RecipeHelper.java
@@ -8,7 +8,7 @@ import net.minecraft.item.ItemStack;
 public class RecipeHelper {
 	
 	public static void initRecipes() {
-		CraftingVoidHeliograph.getInstance().addVoidRecipe(new ItemStack(ExtraneousItems.saplingAccumulator, 1, 0), new Object[]{"S S", 'S', Block.dirt});
+		
 	}
 	
 	
--- common/astorian/exc/inventory/RecipeSorterVoid.java
+++ common/astorian/exc/inventory/RecipeSorterVoid.java
@@ -7,22 +7,11 @@ import net.minecraft.item.crafting.ShapedRecipes;
 import net.minecraft.item.crafting.ShapelessRecipes;
 
 public class RecipeSorterVoid implements Comparator {
-	
-	final CraftingVoidHeliograph craftingVoid;
-	
-	RecipeSorterVoid(CraftingVoidHeliograph crafting)
-	{
-	         this.craftingVoid = crafting;
-	}
-	public int compareRecipes(IRecipe par1IRecipe, IRecipe par2IRecipe)
-	{
-	         return par1IRecipe instanceof ShapelessRecipes && par2IRecipe instanceof ShapedRecipes ? 1 : (par2IRecipe instanceof ShapelessRecipes && par1IRecipe instanceof ShapedRecipes ? -1 : (par2IRecipe.getRecipeSize() < par1IRecipe.getRecipeSize() ? -1 : (par2IRecipe.getRecipeSize() > par1IRecipe.getRecipeSize() ? 1 : 0)));
-	}
-	
-	@Override
-	public int compare(Object par1Obj, Object par2Obj)
-	{
-	         return this.compareRecipes((IRecipe)par1Obj, (IRecipe)par2Obj);
-	}
 
+	@Override
+	public int compare(Object arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
 }
