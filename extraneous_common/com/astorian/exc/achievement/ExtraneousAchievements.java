0b9148b28bd5afe1e5a6e04214091a37b65f16cd
Astorian <royalgamingarmy@gmail.com>
2013-03-17 15:38:24 +0200

    Achievement get! Achievements!

---
+++ extraneous_common/com/astorian/exc/achievement/ExtraneousAchievements.java
@@ -0,0 +1,32 @@
+package com.astorian.exc.achievement;
+
+import com.astorian.exc.item.ExtraneousItems;
+
+import net.minecraft.stats.Achievement;
+import net.minecraft.stats.AchievementList;
+
+public class ExtraneousAchievements {
+	
+	static Achievement timeAccumulate;
+	
+	public static void init() {
+		
+	}
+	
+	public static void initializeAchievements() {
+		
+		timeAccumulate = new Achievement(2001, "timeAccumulate", 1, -4, ExtraneousItems.herbalAccumulator, null);
+		
+	}
+	
+	public static void registerAchievements() {
+		
+		timeAccumulate.registerAchievement();
+		
+	}
+	
+	public static void conditions() {
+		
+	}
+
+}
