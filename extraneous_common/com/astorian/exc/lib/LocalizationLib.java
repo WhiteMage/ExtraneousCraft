398156a5ecebf852bf395ea787e462c7455b3f29
Astorian <royalgamingarmy@gmail.com>
2013-03-31 12:21:12 +0300

    2 full langs added

--- extraneous_common/com/astorian/exc/lib/LocalizationLib.java
+++ extraneous_common/com/astorian/exc/lib/LocalizationLib.java
@@ -6,7 +6,9 @@ public class LocalizationLib {
 	
 	public static String[] localeFiles = { 
 		LANG_LOCATION + "en_US.xml",
-		LANG_LOCATION + "tr_TR.xml"
+		LANG_LOCATION + "tr_TR.xml",
+                LANG_LOCATION + "de_DE.xml",
+                LANG_LOCATION + "nl_NL.xml"
 	};
 
 			
