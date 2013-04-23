7743065f6e653c35e2ff22404ccb87cb20534322
Astorian <royalgamingarmy@gmail.com>
2013-04-23 23:04:08 +0300

    The more is not always the better ;)
    Testing phase is over getting into some serious developement stage including compatibility...

--- extraneous_common/com/astorian/exc/core/helper/CheckHelper.java
+++ extraneous_common/com/astorian/exc/core/helper/CheckHelper.java
@@ -3,5 +3,25 @@ package com.astorian.exc.core.helper;
 import cpw.mods.fml.common.Loader;
 
 public class CheckHelper {
-	
+
+    public static boolean isIndustrialCraftLoaded() {
+	return Loader.isModLoaded("IC2");
+    }
+
+    public static boolean isThaumCraftLoaded() {
+	return Loader.isModLoaded("Thaumcraft");
+    }
+
+    public static boolean isThermalExpansionLoaded() {
+	return Loader.isModLoaded("ThermalExpansion");
+    }
+
+    public static boolean isForestryLoaded() {
+	return Loader.isModLoaded("Forestry");
+    }
+
+    public static boolean isCompactCraftingLoaded() {
+        return Loader.isModLoaded("CCMain");
+    }
+        
 }
