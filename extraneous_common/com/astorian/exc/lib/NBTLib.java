cf18a06a6972198de465f5f3f58c1c4485742b69
Astorian <royalgamingarmy@gmail.com>
2013-04-26 22:26:34 +0300

    Foundation of nbt

---
+++ extraneous_common/com/astorian/exc/lib/NBTLib.java
@@ -0,0 +1,9 @@
+package com.astorian.exc.lib;
+
+public class NBTLib {
+	
+	public static String MOD_NBT_PREFIX = "excData" + "_";
+	
+	public static String ACCUMULATOR_PREFIX = MOD_NBT_PREFIX + "accumulator" + "_";
+
+}
